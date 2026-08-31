16bf3f56db543e9c9257d7ce9e3d2d1f8fd87808
오창준 <cjbormk11@gmail.com>
2026-09-01 06:14:36 +0900

    feat: 전역 푸터 컴포넌트 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import Header from "../shared/ui/Header/Header";
+import Footer from "../shared/ui/Footer/Footer";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="h-full min-h-screen max-w-full min-w-fit">
+    <div className="h-full min-h-screen max-w-full min-w-fit flex flex-col">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   ),
 });
