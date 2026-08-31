93df2ce60ab209c53236d1267c0ef570e4f0930b
오창준 <cjbormk11@gmail.com>
2026-08-31 19:30:21 +0900

    refactor: 모바일 환경에 맞게 헤더 스타일 조정

--- src/shared/ui/Header/Header.tsx
+++ src/shared/ui/Header/Header.tsx
@@ -1,9 +1,19 @@
+import { useLocation } from "@tanstack/react-router";
 import Logo from "../../assets/icons/Logo/Logo.svg?react";
 
 const Header = () => {
+  const location = useLocation();
+  const isMobileRoute = location.pathname === "/";
+
   return (
     <header className="fixed top-0 right-0 left-0 z-10">
-      <div className="container mx-auto max-w-[834px] p-6">
+      <div
+        className={`w-full mx-auto flex items-center justify-between ${
+          isMobileRoute
+            ? "max-w-[430px] px-4.5 pt-3 pb-6"
+            : "max-w-[834px] md:p-6"
+        }`}
+      >
         <Logo className="w-[59.21px] h-6 text-green-500" />
       </div>
     </header>
