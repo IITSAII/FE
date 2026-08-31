c7a3e1208a2850cc7718c678eda52274b0ab142c
오창준 <cjbormk11@gmail.com>
2026-08-31 18:06:48 +0900

    chore: 토스페이먼츠 SDK 의존성 및 Vite 환경변수 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: ["VITE_", "TOSS_"],
   plugins: [
     tanstackRouter({
       routesDirectory: "./src/routes", // 라우트 파일 위치
